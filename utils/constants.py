8ee5216eec4f5c7de542297fd53cb623237d9ba8
Chanpreet Singh <chanpreets10@gmail.com>
2024-08-07 01:11:21 +0530

    Code for synthetic data generation

---
+++ utils/constants.py
@@ -0,0 +1,8 @@
+import os
+
+# Synthetic data
+total_data_points = 2000
+data_folder = "data"
+synthetic_data_file = os.path.join(data_folder, "synthetic_data.csv")
+
+# MySQL constants
---
+++ utils/excel_utils.py
@@ -0,0 +1,26 @@
+import traceback
+
+import pandas as pd
+
+from utils import file_utils
+
+def read_csv_file(file_path):
+    df = pd.DataFrame()
+    try:
+        df = pd.read_csv(file_path)
+    except Exception as e:
+        print("Error : {0}\nException : {1}".format(e, traceback.format_exc()))
+    return df
+
+def dump_csv_file(data, file_path, need_header=True, need_index=False):
+    status = False
+    try:
+        data.to_csv(file_path, header=need_header, index=need_index)
+        if file_utils.get_file_size(file_path) > 0:
+            status = True
+    except Exception as e:
+        print("Error : {0}\nException : {1}".format(e, traceback.format_exc()))
+    return status
+
+def convert_list_into_dataframe(data_list):
+    return pd.DataFrame(data_list)
